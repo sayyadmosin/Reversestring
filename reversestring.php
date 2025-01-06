a2dca90f526c3fd19b6d8d9e2c7f9e8c11214f7d
Mosin sayyed <mosin143243@gmail.com>
2025-01-06 13:50:15 +0530

    made function

---
+++ reversestring.php
@@ -0,0 +1,11 @@
+<?php
+
+function reverseString($str) {
+    return strrev($str);
+}
+
+// Example usage
+$text = "Hello World!";
+$reversed = reverseString($text);
+echo $reversed; // Outputs: !dlroW olleH
+?>
